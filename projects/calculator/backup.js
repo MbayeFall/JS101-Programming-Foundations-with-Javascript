8adab95ef79cafc5df5134ca38fe193eb4dd9979
Mbaye Fall <mbfall56@gmail.com>
2022-04-03 19:04:53 +0000

    slight tweak

--- projects/calculator/backup.js
+++ projects/calculator/backup.js
@@ -1,6 +1,10 @@
 const MESSAGES = require('./calculator_messages.json');
 const readline = require('readline-sync');
 let language;
+let number1;
+let number2;
+let operation;
+let output;
 let answer;
 
 //Bannerizer from Easy 3 exercise JS101
@@ -53,32 +57,40 @@ function getLanguage() {
   }
 }
 
-function getNumber(message) {
+function getFirstNumber() {
   //User enters first number
-  prompt(displayMessage(message, language));
-  let number = readline.question();
+  prompt(displayMessage('firstNumber', language));
+  number1 = readline.question();
   //When wrong number is entered
-  while (number.trimStart() === '' || invalidNumber(number)) {
+  while (number1.trimStart() === '' || invalidNumber(number1)) {
     prompt(displayMessage('wrongNumber', language));
-    number = readline.question();
+    number1 = readline.question();
   }
-  return number;
+}
+
+function getSecondNumber() {
+  //User enters second number
+  prompt(displayMessage('secondNumber', language));
+  number2 = readline.question();
+  //When wrong number is entered
+  while (number2.trimStart() === '' || invalidNumber(number2)) {
+    prompt(displayMessage('wrongNumber', language));
+    number2 = readline.question();
+  }
 }
 
 function getOperation() {
   //User enters desired operation
   prompt(displayMessage('operation', language));
-  let operation = readline.question();
+  operation = readline.question();
   //When wrong operation is entered
   while (!['1', '2', '3', '4'].includes(operation)) {
     prompt(displayMessage('wrongOperation', language));
     operation = readline.question();
   }
-  return operation;
 }
 
-function performCalculation(operation, number1, number2) {
-  let output;
+function performCalculation() {
   switch (operation) {
     case '1':
       output = Number(number1) + Number(number2);
@@ -90,13 +102,16 @@ function performCalculation(operation, number1, number2) {
       output = Number(number1) * Number(number2);
       break;
     case '4':
-      Number(number2) === 0 ? prompt(displayMessage('division0', language)) : output = Number(number1) / Number(number2);
-      break;
+      if (Number(number2) === 0) {
+        prompt(displayMessage('division0', language));
+      } else {
+        output = Number(number1) / Number(number2);
+        break;
+      }
   }
-  return output;
 }
 
-function displayResult(output, operation, number2) {
+function displayResult() {
   if (!(Number(operation) === 4 && Number(number2) === 0)) {
     prompt(`${displayMessage('result', language)} ${output}`);
   }
@@ -114,11 +129,11 @@ getLanguage();
 welcomeMessage();
 
 while (true) {
-  let number1 = getNumber('firstNumber');
-  let number2 = getNumber('secondNumber');
-  let operation = getOperation();
-  let output = performCalculation(operation, number1, number2);
-  displayResult(output, operation, number2);
+  getFirstNumber();
+  getSecondNumber();
+  getOperation();
+  performCalculation();
+  displayResult();
   askNewCalculation();
   //Stop the program if user doesn't want new calculation
   if (!['y', 'yes'].includes(answer)) {
