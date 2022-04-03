420a7399d81c1f6a6d9efaddcade63e411cded4d
Mbaye Fall <mbfall56@gmail.com>
2022-04-03 20:02:08 +0000

    refactoring code

--- projects/mortgage_calculator/mortgage_calculator.js
+++ projects/mortgage_calculator/mortgage_calculator.js
@@ -7,13 +7,7 @@
 // print the monthly payment with 2 decimals
 
 const readlineSync = require("readline-sync");
-let loanAmount;
-let annualInterestRate;
-let LoanDurationInYears;
-let restart;
-let monthlyInterestRate;
-let LoanDurationInMonths;
-let monthlyPayment;
+
 
 function prompt(msg) {
   console.log(`==> ${msg}`);
@@ -27,58 +21,62 @@ function invalidNumber(number) {
 function getLoanAmount() {
   //Ask for the loan amount
   prompt("What is the loan amount ?");
-  loanAmount = readlineSync.question('$');
+  let loanAmount = readlineSync.question('$');
   // When wrong number is entered
   while (!loanAmount.trimStart('') || invalidNumber(loanAmount)) {
     prompt("Hmm... That seems like an invalid number, please try again !");
     loanAmount = readlineSync.question();
   }
+  return loanAmount;
 }
 function getInterestRate() {
   // Ask for the interest rate
   prompt("What is the annual percentage rate (APR) ?\nEx: Enter \"15\" for 15% ");
-  annualInterestRate = readlineSync.question();
+  let annualInterestRate = readlineSync.question();
   // When wrong number is entered
   while (!annualInterestRate.trimStart('') || invalidNumber(annualInterestRate)) {
     prompt("Hmm... That seems like an invalid number, please try again !");
     annualInterestRate = readlineSync.question();
   }
   annualInterestRate /= 100;
+  return annualInterestRate;
 }
 function getLoanDuration() {
   // Ask for loan duration
   prompt("What is the loan duration (in Years) ?");
-  LoanDurationInYears = readlineSync.question();
+  let loanDurationInYears = readlineSync.question();
   // When wrong number is entered
-  while (!LoanDurationInYears.trimStart('') || invalidNumber(LoanDurationInYears)) {
+  while (!loanDurationInYears.trimStart('') || invalidNumber(loanDurationInYears)) {
     prompt("Hmm... That seems like an invalid number, please try again !");
-    LoanDurationInYears = readlineSync.question();
+    loanDurationInYears = readlineSync.question();
   }
+  return loanDurationInYears
 }
-function performCalculation() {
-  monthlyInterestRate = annualInterestRate / 12;
-  LoanDurationInMonths = LoanDurationInYears * 12;
+function performCalculation(loanAmount, monthlyInterestRate, loanDurationInMonths) {
   monthlyPayment = loanAmount * (monthlyInterestRate / (1 -
-  Math.pow((1 + monthlyInterestRate), (-LoanDurationInMonths))));
+  Math.pow((1 + monthlyInterestRate), (-loanDurationInMonths))));
+  return monthlyPayment;
 }
-function printPaymentAmount() {
+function printPaymentAmount(monthlyPayment) {
   prompt(`The monthly payment amount is $${monthlyPayment.toFixed(2)}`);
 }
 function askNewCalculation() {
   prompt("Would you like to perform a new calculation ?\nEnter \"y\" to continue\nEnter anything else to exit");
-  restart = readlineSync.question().toLowerCase();
+  return readlineSync.question().toLowerCase();
 }
 
 
 // START OF PROGRAM
 printWelcome();
 while (true) {
-  getLoanAmount();
-  getInterestRate();
-  getLoanDuration();
-  performCalculation();
-  printPaymentAmount();
-  askNewCalculation();
+  let loanAmount = getLoanAmount();
+  let annualInterestRate = getInterestRate();
+  let loanDurationInYears = getLoanDuration();
+  let monthlyInterestRate = annualInterestRate / 12;
+  let loanDurationInMonths = loanDurationInYears * 12;
+  let monthlyPayment = performCalculation(loanAmount, monthlyInterestRate, loanDurationInMonths);
+  printPaymentAmount(monthlyPayment);
+  let restart = askNewCalculation();
   if (!["y", "yes"].includes(restart)) break;
   console.clear();
 }
