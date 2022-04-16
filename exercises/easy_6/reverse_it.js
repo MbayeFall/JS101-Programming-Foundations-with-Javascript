9f18ccf2f833c93fa8a785243b257f9593456e44
Mbaye Fall <mbfall56@gmail.com>
2022-04-16 21:26:09 +0000

    easy 6 exercises update

--- exercises/easy_6/reverse_it.js
+++
@@ -1,7 +0,0 @@
-function reverseSentence(string) {
- return string.split(' ').reverse().join(' ');
-}
-
-reverseSentence('');                       // ""
-reverseSentence('Hello World');            // "World Hello"
-reverseSentence('Reverse these words');    // "words these Reverse"
