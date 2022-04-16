4f7b24f12ec1c421f88a01478559901648393afc
Mbaye Fall <mbfall56@gmail.com>
2022-04-16 21:23:59 +0000

    debugging exercises first commit

---
+++ exercises/debugging_js/reserved_keywords.js
@@ -0,0 +1,16 @@
+//the forEach method doesn't return anything. The some method was built specifically for this kind of situation.
+
+const RESERVED_KEYWORDS = ['break', 'case', 'catch', 'class', 'const', 'continue',
+  'debugger', 'default', 'delete', 'do', 'else', 'enum', 'export', 'extends', 'finally',
+  'for', 'function', 'if', 'implements', 'import', 'in', 'instanceof', 'interface',
+  'let', 'new', 'package', 'private', 'protected', 'public', 'return', 'static',
+  'super', 'switch', 'this', 'throw', 'try', 'typeof', 'var', 'void', 'while',
+  'with', 'yield'];
+
+function isReserved(name) {
+  return RESERVED_KEYWORDS.some(reserved => name === reserved);
+}
+
+console.log(isReserved('monkey')); // false
+console.log(isReserved('patch'));  // false
+console.log(isReserved('switch')); // should be: true
